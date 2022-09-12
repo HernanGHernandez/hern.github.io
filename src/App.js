530435f71c1e6af28fe533c100e6a76dc8067f63
HernanHernandez <67992122+HernanGHernandez@users.noreply.github.com>
2022-09-12 14:52:51 -0700

    nav bar done and initial body started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import './styles/NavBar.css'
+import { NavBar } from "./components/NavBar"
+import { Banner } from "./components/Banner"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Banner />
+        <Routes>
+          <Route path='/' />
+          {/* <Route path='/projects' element={<Projects />} />
+        <Route path='/experience' element={<Experience />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import { Navbar, Nav, Button, Container, NavbarBrand } from 'react-bootstrap';
+import "../styles/NavBar.css";
+import { FaEllipsisH } from "react-icons/fa";
+import { TbArrowBack, TbBrandGithub, } from "react-icons/tb";
+import { FiLinkedin, FiInstagram } from "react-icons/fi";
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+
+export const NavBar = () => {
+
+    // class creation = initial state
+    const [click, setClick] = useState(false);
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+    const location = useLocation();
+
+    const handleClick = () => {
+        setClick(!click);
+    }
+
+    useEffect(() => {
+        setClick(!click);
+    }, [location]);
+
+    // Window scroll
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    // active link
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return (
+        <header>
+            <Navbar className={scrolled ? "scrolled" : ""} expand="lg" bg="light" variant="light" id={click ? "open" : "close"} >
+                <Container>
+
+                    {/* Logo */}
+                    <NavbarBrand className='brand-name' href="#home">
+                        Hernan Hernandez
+                    </NavbarBrand>
+
+                    <Navbar.Toggle>
+                        {/* hamburger side button \*/}
+                        <span className='toggleButton' onClick={handleClick}>
+                            {click ? <FaEllipsisH /> : <TbArrowBack />} </span>
+                    </Navbar.Toggle>
+
+                    <Navbar.Collapse>
+                        {/* links for navbar*/}
+                        <Nav className='ms-auto'>
+                            {/* <div className="links"> */}
+                            {/* <div className="navbar-nav ms-auto"> */}
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}> About </Nav.Link>
+                            <Nav.Link href="#education" className={activeLink === 'education' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}> Education </Nav.Link>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}> Projects </Nav.Link>
+                            {/* </div> */}
+                            {/* </div> */}
+                        </Nav>
+                    </Navbar.Collapse>
+
+
+                </Container>
+            </Navbar>
+        </header >
+    )
+}
+
+export default NavBar
