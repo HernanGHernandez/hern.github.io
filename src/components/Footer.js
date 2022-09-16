26833cdc230398746d593737920ef435c00ffc33
HernanHernandez <67992122+HernanGHernandez@users.noreply.github.com>
2022-09-15 18:46:10 -0700

    footer updated and finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { Container, Row, Col } from "react-bootstrap";
-import { AiOutlineDown } from "react-icons/ai";
-import Typewriter from "typewriter-effect";
+import React from 'react';
+import { Container } from "react-bootstrap";
+import { TbBrandGithub, } from "react-icons/tb";
+import { FiLinkedin, FiInstagram } from "react-icons/fi";
+import { AiOutlineMail } from "react-icons/ai";
 import "../styles/Footer.css";
-import hernDrawing from "../assets/img/minimalist-hern.png"
-// import { Link } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
 
 export const Footer = () => {
     return (
         <section class="Footer" id="Footer">
             <Container>
+                <div className='Contact'>
+                    <a href="mailto:hernanghrndz@gmail.com"><AiOutlineMail size={35} /></a>
+                    <span className='dot'>•</span>
+                    <a href="https://www.linkedin.com/in/HernanHrndz"> <FiLinkedin size={35} /></a>
+                    <span className='dot'>•</span>
+                    <a href="https://www.instagram.com/hernanh_"> <FiInstagram size={35} /></a>
+                    <span className='dot'>•</span>
+                    <a href="https://www.github.com/HernanGHernandez"> <TbBrandGithub size={35} /></a>
+                </div>
                 <div className='footerSection'>
                     <p>©2022 Copyright: Hernan Hernandez</p>
                 </div>
             </Container>
-            {/* <div class="container row" id="social-container">
-                <a href="https://www.linkedin.com/in/christine-nguyenl/" target="_blank">LinkedIn</a>
-                <span>•</span>
-                <a href="https://github.com/chrlng" target="_blank">Github</a>
-                <span>•</span>
-                <a href="https://www.instagram.com/heyythereitschristine/?hl=en" target="_blank">Instagram</a>
-                <span>•</span>
-                <a href="mailto:christine_nguyenl@berkeley.edu" target="_blank">Email</a>
-            </div>
-            <div>
-                <p id="copyright">Coded by Christine with ♡ 2020</p>
-            </div> */}
         </section>
     )
 }
